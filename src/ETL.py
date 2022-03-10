6042b8db4b3db16e024c930b1693eed1b3613bce
pwuest <philippe.wuest@accuracy.com>
2022-03-10 13:30:00 +0100

    commit 10 03

--- src/ETL.py
+++ src/ETL.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from dateutil.relativedelta import relativedelta
+from pandas.tseries.offsets import MonthEnd
+
 import config
 import re
 import pickle
@@ -21,7 +24,7 @@ def extract_datatape(datatape_path):
     return df
 
 
-def create_palier(df: pd.DataFrame, cutoff_date, npal: int = 4):
+def create_palier(df: pd.DataFrame, npal: int = 4):
 
     def _sub_pal_num(data_):
         col_pal = [col for col in df if 'PAL' in col]
@@ -33,9 +36,18 @@ def create_palier(df: pd.DataFrame, cutoff_date, npal: int = 4):
                      for i in range(1, npal+1)])
 
     PAL = PAL.rename(columns={'FUNIIMS_DT_PREM_ECH_PAL': 'PAL_DStart',
-                              'FUNIIMS_MT_MENS_PAL': 'Mensualite'})
+                              'FUNIIMS_MT_MENS_PAL': 'Mensualite',
+                              'FUNIIMS_NBR_MENS_PAL': 'NbrMensualites'})
 
-    PAL = PAL.dropna(subset='PAL_DStart')
+    PAL = PAL.dropna(subset='PAL_DStart').sort_values(by=['NDOSS', 'PAL'], ascending=False)
+    PAL['NbrMensualites'] = PAL['NbrMensualites'].fillna(0)
+
+    # Fin de plan
+    FdP = PAL.copy().groupby('NDOSS').first().reset_index()
+    FdP['DateFinPlan'] = FdP.apply(lambda x: x['PAL_DStart'] + relativedelta(months=max(int(x['NbrMensualites'])-1, 0))
+                                             + MonthEnd(0), axis=1)
+
+    PAL                = pd.merge(PAL, FdP[['NDOSS', 'DateFinPlan']], on='NDOSS')
 
     return PAL.sort_values(by=['PAL_DStart', 'NDOSS'])
 
@@ -45,15 +57,19 @@ def create_model(df: pd.DataFrame, pal: pd.DataFrame, col_dates):
     model = df.join(pd.DataFrame(col_dates).rename(columns={0: 'DateProj'}), how='cross')\
               .sort_values(by=['DateProj', 'NDOSS'])
 
-    model = pd.merge_asof(model, pal[['NDOSS', 'PAL_DStart', 'Mensualite']],
+    model = pd.merge_asof(model, pal[['NDOSS', 'PAL_DStart', 'Mensualite', 'DateFinPlan']],
                          left_on='DateProj', right_on='PAL_DStart', by='NDOSS').drop(columns=['PAL_DStart'])
+
+    model.loc[model['DateProj'] > model['DateFinPlan'], 'Mensualite'] = 0
+    model['DateFinPlan'] = model.groupby('NDOSS')[['DateFinPlan']].fillna(method='bfill')
+    model['Mensualite']  = model['Mensualite'].fillna(0)
 
     return model
 
 
-def output_model(model: pd.DataFrame, col_index, col_dates):
+def output_model(model: pd.DataFrame, col_index, col_dates, cutoff_date):
 
-    model[col_index] = model[col_index].replace(pd.NaT, pd.to_datetime('today')).fillna('NAN')
+    model[col_index] = model[col_index].replace(pd.NaT, pd.to_datetime('1900-01-01')).fillna('NAN')
     out_ = pd.pivot_table(model,
                           values='Mensualite',
                           index=col_index,
@@ -61,10 +77,20 @@ def output_model(model: pd.DataFrame, col_index, col_dates):
                           aggfunc=np.nansum,
                           fill_value=0,
                           observed=True,
-                          ).reset_index().replace("NAN", np.nan)
+                          ).reset_index().replace("NAN", np.nan).replace(pd.to_datetime('1900-01-01'), None)
 
-    out_['Mens_total']      = out_[col_dates].sum(axis=1)
-    out_['Sum Mens - CRD']  = out_['Mens_total'] - out_['CTX_MCRD']
+    out_['Mens_total']            = out_[col_dates].sum(axis=1)
+    out_['Check Sum Mens - CRD']  = out_['Mens_total'] - out_['CTX_MCRD']
+    out_['DateDebutPlan']         = out_['FUNIIMS_DT_PREM_ECH_PAL1']
+    out_['Seasoning']             = (cutoff_date - out_['DateDebutPlan'])/np.timedelta64(1, 'M')
+    out_['Seasoning']             = out_['Seasoning'].fillna(0).apply(np.floor).astype(int)
+
+    # reshuffle and rename columns
+    out_ = out_[[col for col in out_.columns if col not in col_dates] + col_dates]
+    out_ = out_.rename(columns=dict(zip(col_dates, [dat.date() for dat in col_dates])))
+
+    col_dat = [col for col in out_.columns if 'DT_' in str(col)] + ['CTX_DDERN_ENCAISS', 'DateFinPlan', 'DateDebutPlan']
+    out_[col_dat] = out_[col_dat].apply(lambda x: pd.to_datetime(x, errors='coerce', dayfirst=True).dt.date)
 
     return out_
 
@@ -83,8 +109,14 @@ if __name__ == "__main__":
         dt = pickle.load(f)
         f.close()
 
-    paliers     = create_palier(dt, config.dCutoff)
+    paliers         = create_palier(dt)
 
-    model       = create_model(dt, paliers, config.col_dates)
+    modelCF         = create_model(dt, paliers, config.col_dates)
 
-    output      = output_model(model, list(dt.columns), config.col_dates)
+    output          = output_model(modelCF,
+                                   list(dt.columns) + ['DateFinPlan'],
+                                   config.col_dates, config.dCutoff)
+
+    print("Done")
+
+    output.to_excel(config.path_result, index=False, sheet_name="Model")
--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from types import SimpleNamespace
 
 import pandas as pd
 from dateutil.relativedelta import relativedelta
-from pandas._libs.tslibs.offsets import MonthEnd
+from pandas.tseries.offsets import MonthEnd
 
 SRC_DIR     = os.path.dirname(__file__)
 ROOT_DIR    = os.path.join(SRC_DIR, os.pardir)
@@ -11,14 +11,16 @@ DATA_DIR    = os.path.join(ROOT_DIR, 'data')
 
 dCutoff     = '2021-12-31'
 dCutoff     = pd.to_datetime(dCutoff)
-nHorizon    = 20
+nHorizon    = 300
 
 col_dates = [dCutoff + relativedelta(months=i) + MonthEnd(0) for i in range(1, nHorizon+1)]
+# col_dates = [dat.date() for dat in col_dates]
 
 datatape_path = SimpleNamespace(
     io=os.path.join(DATA_DIR, 'Presentation_20220303_fichier cession sdt 2022 via CRE CRI.xlsx'),
     sheet_name='CESSION_SDT_2022'
 )
 
-path_cache = os.path.join(DATA_DIR, 'datatape.pkg')
+path_cache      = os.path.join(DATA_DIR, 'datatape.pkg')
+path_result     = os.path.join(DATA_DIR, 'model.xlsx')
 
